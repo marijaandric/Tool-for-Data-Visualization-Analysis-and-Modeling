850d4589c6dcccaf5043c38f8e2a6fc737e8e98a
Marija Andric <marijaandric2001@gmail.com>
2024-11-15 00:43:06 +0100

    Create navbar layout

--- front/src/components/general/Navbar/Navbar.tsx
+++ front/src/components/general/Navbar/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div className="card-container">"hi"</div>;
+  return (
+    <div className="navbar-content">
+      <h1>Title</h1>
+      <div>
+        <p>Name Lastname</p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- front/src/pages/Layout/Layout.tsx
+++ front/src/pages/Layout/Layout.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/general/Sidebar/Sidebar.tsx";
 import "./Layout.css";
+import Navbar from "../../components/general/Navbar/Navbar.tsx";
 
 const Layout = () => {
   return (
     <div className="app-layout">
       <div className="content-wrapper">
         <Sidebar />
-        <main className="main-content">
-          <Outlet />
-        </main>
+        <div className="main-content">
+          <Navbar />
+          <main>
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   );
